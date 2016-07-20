fe0cdf17c08a623d7ce2eb9cc0b33058525eb7e4
Chris Kuh <kuh_chris@yahoo.com>
2016-07-19 19:59:35 -0700

    [fix] remove retweets when fetching tweets

--- server/models/twitter.model.js
+++ server/models/twitter.model.js
@@ -4,13 +4,16 @@ var _ = require('underscore');
 exports.get = function(query) {
   console.log('inside twitter.model.js get')
   return new Promise(function(resolve, reject) {
-    Twitter.get('statuses/user_timeline', {screen_name: query.userName, count: query.count || 10}, function(error, tweets, response) {
+    Twitter.get('statuses/user_timeline', {screen_name: query.userName, count: query.count}, function(error, tweets, response) {
       if(error){
         reject(error)
       } else {
-        resolve(_.map(tweets, function(tweet) {
-          return _.pick(tweet, 'created_at', 'text', 'user', 'retweeted_status')
-        }))
+        var filteredTweets = _.map(tweets, function(tweet) {
+          return _.pick(tweet, 'created_at', 'text', 'user', 'retweeted_status');
+        })
+        resolve(_.reject(filteredTweets, function(currentTweet){
+          return 'retweeted_status' in currentTweet;
+        }));
       }
     })
   });
